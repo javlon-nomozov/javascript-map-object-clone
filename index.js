0c9d81cf0220c608cfd82fe50ad7d4e9bbe93c34
Joe McNeil <69708014+samarqandiy@users.noreply.github.com>
2023-11-11 13:41:40 +0300

    Update index.js
    hi

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const { MyMap } = require("./src/map");
 
+console.log();
+console.log();
+
 const mm2 = new MyMap();
-mm2.set(true, true);
-mm2.set(false, false);
+console.log(mm2.set(true, true));
+console.log(mm2.set(false, false));
 console.log(mm2.delete("true"));
 console.log(mm2.entries());
 console.log(mm2.data);
+
+console.log();
+console.log();
+
+const mm = new Map();
+console.log(mm.set(true, true));
+console.log(mm.set(false, false));
+console.log(mm.delete("true"));
+console.log(mm.entries());
+console.log(mm.data);
